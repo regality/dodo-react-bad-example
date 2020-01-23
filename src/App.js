9c216bbd5a70a4ca50dd9d47cd93241268bddb1f
Adam Blackburn <regality@gmail.com>
2020-01-23 10:56:51 -0700

    dodo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  onAddTweet (tweet) {
+    this.setState({ newTweet: tweet })
+  }
+
+  onTweetAdded () {
+    this.setState({ newTweet: null })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <AddTweet onAddTweet={this.onAddTweet.bind(this)} />
+          <TweetList newTweet={this.state.newTweet} onTweetAdded={this.onTweetAdded.bind(this)} />
+        </header>
+      </div>
+    )
+  }
+}
+
+class AddTweet extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return <div>
+      <input value={this.state.tweet} onChange={e => this.setState({ tweet: e.target.value })} />
+      <button onClick={this.props.onAddTweet.bind(this, this.state.tweet)}>tweet!</button>
     </div>
-  );
+  }
+}
+
+class TweetList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      tweets: [ 'first tweet' ]
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.newTweet) {
+      axios('/add', { tweet: nextProps.newTweet }).then(() => {
+        this.props.onTweetAdded()
+        this.setState({ tweets: [ ...this.state.tweets, nextProps.newTweet ] })
+      })
+    }
+  }
+
+  render () {
+    return <ul>
+      {this.state.tweets.map(tweet => <li>{tweet}</li>)}
+    </ul>
+  }
 }
 
 export default App;
+
+
+// fake ajax helper
+function axios(url) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, 100)
+  })
+}
